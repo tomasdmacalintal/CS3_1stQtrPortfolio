b84b476ae42fff69bb1a6aa21cf710d17e42a3d4
tomasdmacalintal <b2029dptdmacalintal@pshs.edu.ph>
2025-09-09 14:31:44 +0800

    Create test.js, test.html

---
+++ test.js
@@ -0,0 +1,44 @@
+/*let s = ""
+for (let i = 1; i <=10; i++){
+    for (let j = 0; j < 10; j++){
+        s += i + j * 10 + "  ";
+    }
+    s += "\n";
+}
+
+console.log(s);*/
+
+/*for (let i = 1; i <= 20; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}*/
+
+/*
+let i = 1;
+do{
+    console.log(i);
+    ++i;
+} while(i<=3)*/
+
+/*lebron = ["You","are","my","sunshine"];
+let myWords = "";
+for (let i = 0; i < lebron.length; i++) {
+    myWords += lebron[i] + " ";
+}
+console.log(myWords);*/
+
+/*grades = [99,100,97,82,96];
+for (let i = 0; i < grades.length; i++) {
+    console.log(grades[i]);
+}
+
+for (let grade of grades) {
+    console.log(grade);
+}
+
+grades.forEach(print_grade);
+
+function print_grade(grade){
+    console.log(grade)
+}*/
